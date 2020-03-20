1459be49e361a16ab19110882cd127f6310a9f87
Joshua Brutscher <joshuabrutscher@Joshuas-MacBook-Pro.local>
2020-03-21 00:25:20 +0100

    Added attribute name.

--- corona/mainapp/models.py
+++ corona/mainapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    """
+    name: 
+    """
+    name = models.CharField(max_length=50, unique=True, primary_key=True)
