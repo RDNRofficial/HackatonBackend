af14abaf95eb0826ec542f61b36832ba975a81bf
Robert Krause <uflfu@student.kit.edu>
2020-03-21 17:27:37 +0100

    started writing setup script

---
+++ corona/setup.py
@@ -0,0 +1,68 @@
+import django
+import os
+import sys
+import random
+from mainapp.models import *
+
+
+tables = [Question, Answer, Userscore, DIYManual,Explanation]
+QUESTIONS = []
+ANSWERS = []
+USER_SCOES = []
+DIY_MANUALS = []
+EXPLANATIONS = []
+
+
+
+def delete_tables():
+    print("Delete all")
+    for table in tables:
+        for o in table.objects.all():
+            o.delete()
+
+
+def save(model, modelName, list):
+    if len(model.objects.all()) == 0:
+        print("Add "+modelName+" Object")
+        for m in list:
+            m.save()
+
+
+def create_question():
+    save(Question, "Question", QUESTIONS)
+
+
+def create_answer():
+    save(Answer, "Answer", ANSWERS)
+
+
+def create_userscore():
+    save(Userscore, "Userscore", USER_SCORES)
+
+
+def create_diymanual():
+    save(DIYManual, "DIYManual", DIY_MANUALS)
+
+
+def create_explanation():
+    save(Explanation, "Explanation", EXPLANATIONS)
+
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "corona.settings")
+    django.setup()
+    generate()
+
+
+def generate():
+    print("make migrations\n")
+    os.system("python manage.py makemigrations")
+    print("migrate\n")
+    os.system("python manage.py migrate")
+    delete_tables()
+    create_question()
+    create_answer()
+    create_userscore()
+    create_diymanual()
+    create_explanation()
+    print("DONE\n")
