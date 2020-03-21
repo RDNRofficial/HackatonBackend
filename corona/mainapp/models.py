372225e4ca065460c4d116a149a58737b97f56b5
Robert Krause <uflfu@student.kit.edu>
2020-03-21 09:07:47 +0100

    started answer model

--- corona/mainapp/models.py
+++ corona/mainapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Answer(models.Model):
+    voher = ImageField()
+    nachher = ImageField()
+    solution = BooleanField()
+    question = models.ForeignKey(Question, related_name='', on_delete=models.CASCADE)
+
+
+class Question(models.Model):
+
+
+class Userscore(models.Model):
+
+
