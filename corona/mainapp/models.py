417894b614434e5e1f188232529f0933bd04cf4f
Joshua Brutscher <joshuabrutscher@Joshuas-MacBook-Pro.local>
2020-03-21 09:29:40 +0100

    Added first serializer and view example.

--- corona/mainapp/models.py
+++ corona/mainapp/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class Answer(models.Model):
-    voher = ImageField()
-    nachher = ImageField()
-    solution = BooleanField()
+    voher = models.ImageField()
+    nachher = models.ImageField()
+    solution = models.BooleanField()
     question = models.ForeignKey(Question, related_name='', on_delete=models.CASCADE)
 
 
 class Question(models.Model):
-
+    pass
 
 class Userscore(models.Model):
-
+    pass
