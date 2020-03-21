6c072129b9d95cc09a41dd7e9d6c2ad8c46cad53
Joshua Brutscher <joshuabrutscher@Joshuas-MacBook-Pro.local>
2020-03-21 10:20:32 +0100

    Changed url setting.

--- corona/mainapp/url.py
+++ corona/mainapp/url.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+
+
 from .views import *
 
 urlpatterns = [
     url(r'answer/$', ListAnswer.as_view()),
     url(r'question/$', ListQuestion.as_view()),
-
 ]
