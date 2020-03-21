dd0c9f133152e996193a9d65d33239cf5a1a81d5
Robert Krause <uflfu@student.kit.edu>
2020-03-21 09:30:47 +0100

    implemented models

--- corona/mainapp/models.py
+++ corona/mainapp/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
-class Answer(models.Model):
-    voher = ImageField()
-    nachher = ImageField()
-    solution = BooleanField()
-    question = models.ForeignKey(Question, related_name='', on_delete=models.CASCADE)
-
-
 class Question(models.Model):
+    frage = models.CharField(max_length=100)
+    background = models.ImageField()
+    audio = models.CharField(max_length=100) #TODO
 
+
+class Answer(models.Model):
+    voher = models.ImageField()
+    nachher = models.ImageField()
+    solution = models.BooleanField()
+    question = models.ForeignKey(Question, related_name='question', on_delete=models.CASCADE)
+    x = models.IntegerField()
+    y = models.IntegerField()
 
 class Userscore(models.Model):
+    score = models.IntegerField()
+    username = models.CharField(max_length=30 ,unique=True)
 
